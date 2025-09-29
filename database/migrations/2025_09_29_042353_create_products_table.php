49e30d746f6e63c6b806ead6120f12c0245f8abf
Студенческая <@rbiu.local>
2025-09-29 09:50:29 +0500

    Classwork_1

---
+++ database/migrations/2025_09_29_042353_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('descripshion')->nullable();
+            $table->float('prise')->default(0);
+            $table->string('amount')->default(0);
+            $table->foreignId('category_id')
+                ->nullable()
+                ->constrained()
+                ->cascadeOnUpdate()
+                ->nullOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
